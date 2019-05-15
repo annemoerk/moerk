cc6e3f1611eda8e00ce6d5c458c19a02223e58b7
annemoerk <48016188+annemoerk@users.noreply.github.com>
2019-05-15 09:14:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('white');
+            }
+
+            else {
+                  $('nav').removeClass('white');
+            }
+      })
+
+// Smooth Scrolling
+$('.cf a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
